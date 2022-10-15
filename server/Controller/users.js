8b0f6e88cf6c25d085de4d1f1a852290a5925d07
TerryTian21 <terry.tian.2003@gmail.com>
2022-10-15 07:25:14 -0400

    Start of JWT

--- server/Controller/users.js
+++ server/Controller/users.js
@@ -1,6 +1,10 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from 'bcryptjs';
+import asyncHandler from "express-async-handler";
 
-export const getUsers = async (req, res) => {
+
+export const getUsers = asyncHandler (async (req, res) => {
     try {
         const users = await User.find();
 
@@ -8,22 +12,65 @@ export const getUsers = async (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
-}
+})
 
-export const createUser = async (req, res) => {
-    const user = req.body;
+export const createUser = asyncHandler (async (req, res) => {
 
-    const newUser = new User(user);
-    try {
-        await newUser.save();
+    const username = req.body.username
+    const password = req.body.password
+    const gender = req.body.gender
+    const sizing = req.body.sizing
 
-        res.status(201).json(newUser);
-    } catch (err) {
-        res.status(409).json({ message: err.message })
+    const userExists = await User.findOne({username})
+
+    // if(userExists){
+    //     res.status(400).json("User already exists")
+    // }
+    
+    //Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+   // Create user
+    const user = await User.create({
+        username,
+        password: hashedPassword,
+        gender,
+        sizing
+    })
+
+    if (user) {
+        res.status(201).json({
+        _id: user.id,
+        name: user.username,
+        gender: user.gender,
+        sizing: user.sizing
+        })
+    } else {
+        res.status(400).json('Invalid user data')
     }
-}
+})
 
-export const getUserId = async (req, res) => {
+export const loginUser = asyncHandler (async (req, res) =>{
+    const username = req.body.username
+    const password = req.body.password
+
+    const user = await User.findOne({username})
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.username,
+            gender: user.gender,
+            sizing: user.sizing,
+        })
+    } else{
+        res.status(400).json("Invalid Credentials")
+    }
+
+})
+
+export const getUserId = asyncHandler (async (req, res) => {
     try {
         const user = await User.findById(req.params.id)
 
@@ -31,9 +78,9 @@ export const getUserId = async (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
-}
+})
 
-export const deleteUser = async (req, res) => {
+export const deleteUser = asyncHandler (async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id)
         
@@ -41,9 +88,9 @@ export const deleteUser = async (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message })
     }
-}
+})
 
-export const updateUser = async (req, res) => {
+export const updateUser = asyncHandler (async (req, res) => {
 
     try{
 
@@ -63,8 +110,7 @@ export const updateUser = async (req, res) => {
     } catch(error){
         res.status(404).json({ message: error.message })
     }
-
     
-}
+})
 
  
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,50 +1,16 @@
 import express from "express";
-import { getUsers, createUser, getUserId, deleteUser, updateUser} from "../Controller/users.js";
+import { getUsers, createUser, getUserId, deleteUser, updateUser, loginUser} from "../Controller/users.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.get("/:id", getUserId);
 router.post("/add", createUser);
-router.delete("/:id", deleteUser)
-router.put('/update/:id', updateUser)
+router.delete("/:id", deleteUser);
+router.post("/login", loginUser); 
+router.put('/update/:id', updateUser);
 
 export default router;
 
 
-
-// Get Request
-// router.route("/").get((req, res) => {
-//   User.find()
-//     .then((users) => res.json(users))
-//     .catch((err) => res.status(400).json("Error: " + err));
-// });
-
-// Post request
-// router.route("/add").post((req, res) => {
-//   const username = req.body.username;
-//   const password = req.body.password;
-//   const gender = req.body.gender;
-//   const sizing = req.body.sizing;
-
-//   const newUser = new User({ username, password, gender, sizing });
-
-//   newUser
-//     .save()
-//     .then(() => res.json("User added!"))
-//     .catch((err) => res.status(400).json("Error: " + err));
-// });
-
-// router.route('/:id').get((req, res) => {
-//     User.findById(req.params.id)
-//       .then(exercise => res.json(exercise))
-//       .catch(err => res.status(400).json('Error: ' + err));
-//   });
-  
-  // Delete
-  // router.route('/:id').delete((req, res) => {
-  //   User.findByIdAndDelete(req.params.id)
-  //     .then(() => res.json('User deleted.'))
-  //     .catch(err => res.status(400).json('Error: ' + err));
-  // });
   
