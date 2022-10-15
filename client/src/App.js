3d414ff44763b7bbf5110c8ea6e4b59157b3a8fa
Yue Fung Lee <yuefungalt@gmail.com>
2022-10-15 11:37:33 -0400

    Gallery View for Home

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,16 @@ import logo from "./logo.svg";
 import "./App.css";
 import Landing from "./components/pages/Landing/Landing";
 import { UserProvider } from "./context/UserContext";
+import SwipePage  from "./components/pages/Home/SwipePage";
+import Home from "./components/pages/Home/Home";
+import Onboarding from "./components/pages/Onboarding/Onboarding";
 
 function App() {
   return (
     <UserProvider>
-      <Landing />
+      {/* <Landing /> */}
+      <Home />
+      {/* <Onboarding /> */}
     </UserProvider>
   );
 }
--- client/src/components/pages/Home/SwipePage.jsx
+++ client/src/components/pages/Home/SwipePage.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { Component } from 'react';
+import { useState } from 'react';
+import Card from '../../Card';
+import Gallery from '../../Gallery';
 
 function SwipePage () {
+    const [userStyles, setStyles] = useState([])
     return (
-       <h1>SwipePage</h1>
-      );
+        <div className='flex flex-col items-center justify-evenly h-150px overflow-auto text-justify p-5'>
+            <h1 className='text-cafe-noir font-bold text-3xl'>Possible Styles You will Like</h1>
+            <div>
+
+            </div>
+            <div className="bg-cultured w-2/6 h-30 overflow-auto m-5 text-justify p-5 rounded-3xl">
+                <Gallery userStyles={userStyles} setStyles={setStyles} />
+            </div>
+            <button className="w-1/4 bg-camel text-white font-serif text-xl rounded-3xl p-2">Continue</button>
+        </div>
+    );
 }
 
 export default SwipePage;
