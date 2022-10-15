59d047b579627d08317a5c7b1da7552cae5793f0
TerryTian21 <terry.tian.2003@gmail.com>
2022-10-14 23:56:30 -0400

    Routes to DB setup

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-let User = require("../collections/user.model");
+let User = require("../models/user.model");
 
 // Get Request
 router.route("/").get((req, res) => {
