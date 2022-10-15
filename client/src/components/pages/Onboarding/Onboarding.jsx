87ae0344b0177f9f7736c81440afbec352ae2f60
juanpabloacosta <acosta.jp@icloud.com>
2022-10-15 04:31:51 -0400

    Added style images

---
+++ client/src/components/pages/Onboarding/Onboarding.jsx
@@ -0,0 +1,10 @@
+const Onboarding = () => {
+    const styles = ["Minimalist", "Edgy", "Casual", "Business Casual", "Vintage", "Biker", "Athletic", "Skater", "Artsy", "Classic", "Cosplay", "Elegant", "Wild West", "Military Fashion", "Nerdy",  "Traditional", "KPOP", "Rave", "Boho", "Hippie"];
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default Onboarding;
