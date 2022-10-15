e5a16cb7dae881fc01a30aa734c649460cdf85ae
dannylu03 <danny.lu075@gmail.com>
2022-10-15 07:09:15 -0400

    Hooked fe to be for registermodal component

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
@@ -26,5 +26,5 @@ app.use("/users", usersRouter);
 app.use("/cards", cardsRouter);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
-  });
+  console.log(`Server is running on port: ${port}`);
+});
