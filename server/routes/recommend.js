2cedba679749eae563ca26575ca512579fbea1cc
Yue Fung Lee <yuefungalt@gmail.com>
2022-10-16 02:27:01 -0400

    client

--- server/routes/recommend.js
+++ server/routes/recommend.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { recommendOutfits } from "../Controller/recommend";
+import { recommendOutfits } from "../Controller/recommend.js";
 
 const router = express.Router();
 router.post("/recommendOutfit", recommendOutfits);
