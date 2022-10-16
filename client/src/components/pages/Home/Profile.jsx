77d9f0661aa2e9afd36121fb47ba90a8ce4bb52e
juanpabloacosta <acosta.jp@icloud.com>
2022-10-16 10:06:20 -0400

    Sample data

--- client/src/components/pages/Home/Profile.jsx
+++ client/src/components/pages/Home/Profile.jsx
@@ -4,7 +4,7 @@ import Card from './Card'
 
 const Profile = () => {
   return (
-    <VStack justifyContent={'center'} height={'100vh'}>
+    <VStack justifyContent={'center'} height={'100vh'} width={'full'}>
         <h1 className='font-bold text-cafe-noir text-3xl p-2'>Profile</h1>
         <Image shadow={'lg'} width={[50, 100, 150]} borderRadius={'full'} objectFit='cover' src="https://bit.ly/sage-adebayo" alt="Segun Adebayo"/>
         <h1 className='font-bold text-cafe-noir text-2xl p-2'>Your Styles</h1>
@@ -12,43 +12,33 @@ const Profile = () => {
           <Grid templateColumns='repeat(2, 1fr)' gap={8} padding={'8'}>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/0-minimalist.jpeg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/juan.jpg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/terry.jpg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/danny.jpg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/friends1.jpg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/friends2.jpg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/16-kpop.jpeg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/12-wild-west.jpeg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/11-elegant.jpeg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
               <div className={"relative h-full w-full rounded-xl overflow-hidden mx-1 transition-all duration-500 ease"}>
                 <img src={require(`../../../assets/image/4-vintage.jpeg`)} alt={``} className="rounded-xl h-auto w-full" />
-                <div className="absolute top-4 z-10 inline-block m-auto text-white text-sm bg-camel rounded-xl p-2">Minimalist</div>
               </div>
 
           </Grid>
--- client/src/components/pages/Home/SideBarSection.jsx
+++ client/src/components/pages/Home/SideBarSection.jsx
@@ -4,7 +4,7 @@ import Profile from "./Profile";
 
 function SideBarSection ({section}) {
     return (
-    <div className="w-75 h-auto m-0 ml-16 overflow-scroll top-0 left-0 bg-timberwolf">
+    <div className="w-5/6 h-auto m-0 ml-16 overflow-scroll top-0 left-0 bg-timberwolf">
         {section === 'profile' ? <Profile /> : <Liked />}
     </div>
 
