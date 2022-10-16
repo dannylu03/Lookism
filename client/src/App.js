2e9b062184ac66646b3814cda8737385dce4812a
Yue Fung Lee <yuefungalt@gmail.com>
2022-10-15 23:46:08 -0400

    Fixed Home Styling

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,35 @@ import Landing from "./components/pages/Landing/Landing";
 import Onboarding from "./components/pages/Onboarding/Onboarding";
 import Home from "./components/pages/Home/Home"
 import { useState } from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import Profile from "./components/pages/Home/Profile";
+
+const colors = {
+  brand: {
+    'ash-gray': '#C6C8BB',
+    'camel': '#CAA473',
+    'timberwolf': '#dad0c7',
+    'cultured': '#ededed',
+    'cafe-noir': '#443220',
+  },
+};
+
+const theme = extendTheme({ colors });
 
 function App() {
   const [user, setUser] = useState();
 
   return (
-    <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      {/* <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing setUser={setUser} />} />
-        <Route path="/onboarding" element={<Onboarding user={user} />} />
-        <Route path="/home" element={<Home user={user} />} />
-      </Routes>
-    </BrowserRouter>
+          <Route path="/" element={<Landing setUser={setUser} />} />
+          <Route path="/onboarding" element={<Onboarding user={user} />} />
+          <Route path="/home" element={<Home user={user} />} />
+        </Routes>
+      </BrowserRouter> */}
+      <Home />
+    </ChakraProvider>
   );
 }
 
--- client/src/components/pages/Home/Home.jsx
+++ client/src/components/pages/Home/Home.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import SideBar from "./SideBar";
 import SideBarSection from "./SideBarSection";
 import SwipePage from "./SwipePage";
+import {HStack } from "@chakra-ui/react";
 
 
 function Home () {
+    const [section, setSection] = React.useState('profile');
+
     return (
-        <div>
-            <SideBar />
-            <SideBarSection />
-            <SwipePage />
-        </div>
+        <HStack>
+            <SideBarSection section={section} />
+            <SideBar section={section} setSection={setSection} />
+            {/* <SwipePage /> */}
+        </HStack>
     );
 }
 
